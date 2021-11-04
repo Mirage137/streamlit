8d7cc5b41ababdc04d130cfff375162783b9566a
mludwig <mjwjludwig@gmail.com>
2021-11-04 11:12:17 -0400

    update

--- ring.py
+++ ring.py
@@ -3,10 +3,10 @@ import pickle
 import joblib
 import pandas as pd
 
-model = joblib.load('./best_nb_model.pkl')
-print("model loaded")
+model = joblib.load('../models/best_logit_model_188.pkl')
 
 @st.cache()
+
 def prediction_threshold(text):
 
     pred_subr        = model.predict(pd.Series(text))
@@ -16,17 +16,27 @@ def prediction_threshold(text):
         if pred_subr[0] == 1:
             subr = "depressed"
         else:
-            subr = "anxiety"
+            subr = "anxious"
     else:
         subr = "indeterminite"
 
     return subr
 
+
+html = """
+<div style ="background-color:#CF6965;padding:14px;border-radius:14px;">
+<h1 style ="color:lightgrey;text-align:center;font-size:56px;">
+SUPPORT VECTOR
+</h1>
+</div>
+"""
+
+st.markdown(html, unsafe_allow_html = True)
 st.title("How are you feeling?")
 
-page = st.sidebar.selectbox("Select a page", ("About", "Make a prediction"))
+page = st.selectbox("Select a page",("Make a prediction", "Seeking Help"))
 
-if page == "About":
+if page == "Seeking Help":
         st.write("Can we guess how you're feeling?")
 
 if page == "Make a prediction":
